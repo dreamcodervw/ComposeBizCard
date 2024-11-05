8de2b87a5d33cf37d6d04c7d1451abbce13c4d07
Milan Bhatt <dreamcodervw@gmail.com>
2024-11-05 19:17:20 +0530

    Sample project completion

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # ComposeBizCard
+
+This repository consists sample project done as a part of Udemy Android Jetpack Compose course
+
+###### Compose components
+
+- Theme
+- Composable function
+- Preview
+- Surface
+- Column
+- Row
+- Text
+- Button
+- Card
+- Modifiers
+- MutableState
+- Remember
+- Image
+- LazyColumn
+- Box
+- HorizontalDivider
--- app/src/main/java/com/dreamcoder/composebizcard/MainActivity.kt
+++ app/src/main/java/com/dreamcoder/composebizcard/MainActivity.kt
@@ -1,29 +1,52 @@
 package com.dreamcoder.composebizcard
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dreamcoder.composebizcard.ui.theme.ComposeBizCardTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ComposeBizCardTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    CreateBizCard()
                 }
             }
         }
@@ -31,17 +54,141 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun CreateBizCard() {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            colors = CardDefaults.cardColors(Color.White),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateImageProfile()
+                HorizontalDivider()
+                CreateInfo()
+                Button(onClick = {
+                    buttonClickedState.value = !buttonClickedState.value
+                }) {
+                    Text("Portfolio")
+                }
+                if (buttonClickedState.value) {
+                    Content()
+                } else {
+                    Box {}
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray),
+            color = Color.White
+        ) {
+            Portfolio(
+                data = listOf(
+                    "Project 1",
+                    "Project 2",
+                    "Project 3",
+                    "Project 4",
+                    "Project 5"
+                )
+            )
+        }
+    }
+}
+
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn {
+        items(data) { item ->
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxWidth(),
+                shape = RectangleShape,
+                colors = CardDefaults.cardColors(Color.White),
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .padding(7.dp)
+                ) {
+                    CreateImageProfile(modifier = Modifier.size(100.dp))
+                    Column(modifier = Modifier
+                        .padding(7.dp)
+                        .align(Alignment.CenterVertically)) {
+                        Text(item, fontWeight = FontWeight.Bold)
+                        Text("A great work!", style = MaterialTheme.typography.bodySmall)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CreateInfo() {
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            "Miles P.",
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(text = "Android compose programmer", modifier = Modifier.padding(3.dp))
+        Text(text = "@themilescompose", modifier = Modifier.padding(3.dp))
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
         modifier = modifier
-    )
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        shadowElevation = 4.dp,
+        border = BorderStroke(0.5.dp, Color.LightGray)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.profile_image),
+            contentDescription = "Profile image"
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeBizCardTheme {
-        Greeting("Android")
+        CreateBizCard()
     }
 }
